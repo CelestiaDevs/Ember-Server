aacd84e0dd8ae4b7364a392c568675198dca3f05
SteelElite <charmsteel1@gmail.com>
2017-01-11 18:14:11 -0500

    Update misc.js

--- plugins/plugins/misc/misc.js
+++ plugins/plugins/misc/misc.js
@@ -1,23 +1,16 @@
 'Use Strict';
 
-/* Original code by panpawn! Modified for roleplau by Prince Sky!*/
-
-var color = require('../config/color');
-hashColor = function(name, bold) {
-	return (bold ? "<b>" : "") + "<font color=" + color(name) + ">" + (Users(name) && Users(name).connected && Users.getExact(name) ? Tools.escapeHTML(Users.getExact(name).name) : Tools.escapeHTML(name)) + "</font>" + (bold ? "</b>" : "");
-}
-
 exports.commands = {
 	credit: 'credits',
 	credits: function (target, room, user) {
 		this.popupReply("|html|" + "<font size=5>Ember Credits</font><br />" +
 					"<u>Owners:</u><br />" +
-					"- " + hashColor('Mia Flores', true) + " (Founder, Sysadmin)<br />" +
-                    "- " + hashColor('Prince Sky', true) + " (Sysadmin, Host, Developer, Lead Policy)<br />" +
+					"- " + Plugins.Colors.apply('Mia Flores', true) + " (Founder, Sysadmin)<br />" +
+                    "- " + Plugins.Colors.apply('Prince Sky', true) + " (Sysadmin, Host, Developer, Lead Policy)<br />" +
 					"<br />" +
 					"<u>Development:</u><br />" +
-					"- " + hashColor('Mia Flores', true) + " (Owner of GitHub repository)<br />" +
-					"- " + hashColor('Prince Sky', true) + " (Owner of Github repository, Server CSS)<br />" +
+					"- " + Plugins.Colors.apply('Mia Flores', true) + " (Owner of GitHub repository)<br />" +
+					"- " + Plugins.Colors.apply('Prince Sky', true) + " (Owner of Github repository, Server CSS)<br />" +
 					"<br />" +
 					"<u>Special Thanks:</u><br />" +
 					"- Current staff team<br />" +
