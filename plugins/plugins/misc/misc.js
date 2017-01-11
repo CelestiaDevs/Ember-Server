162d81a310bfb98f9b27a675267a9f1ead545749
SteelElite <charmsteel1@gmail.com>
2017-01-11 17:55:20 -0500

    Update misc.js

--- plugins/plugins/misc/misc.js
+++ plugins/plugins/misc/misc.js
@@ -1,6 +1,29 @@
 'use strict';
 
+/* Original code by panpawn! Modified for roleplau by Prince Sky!*/
+
+var color = require('../config/color');
+hashColor = function(name, bold) {
+	return (bold ? "<b>" : "") + "<font color=" + color(name) + ">" + (Users(name) && Users(name).connected && Users.getExact(name) ? Tools.escapeHTML(Users.getExact(name).name) : Tools.escapeHTML(name)) + "</font>" + (bold ? "</b>" : "");
+}
+
 exports.commands = {
+	credit: 'credits',
+	credits: function (target, room, user) {
+		this.popupReply("|html|" + "<font size=5>Ember Credits</font><br />" +
+					"<u>Owners:</u><br />" +
+					"- " + hashColor('Mia Flores', true) + " (Founder, Sysadmin)<br />" +
+                   			"- " + hashColor('Prince Sky', true) + " (Sysadmin, Host, Developer, Lead Policy)<br />" +
+					"<br />" +
+					"<u>Development:</u><br />" +
+					"- " + hashColor('Mia Flores', true) + " (Owner of GitHub repository)<br />" +
+					"- " + hashColor('Prince Sky', true) + " (Owner of Github repository, Server CSS)<br />" +
+					"<br />" +
+					"<u>Special Thanks:</u><br />" +
+					"- Current staff team<br />" +
+					"- Our regular users<br />");
+	},
+	
 	globalauth: 'gal',
 	stafflist: 'gal',
 	authlist: 'gal',
