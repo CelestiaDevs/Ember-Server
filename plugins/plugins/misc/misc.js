7dbf4715b95259230c64fa697a250d6a24ee998d
SteelElite <charmsteel1@gmail.com>
2017-01-12 15:47:42 -0500

    Update misc.js

--- plugins/plugins/misc/misc.js
+++ plugins/plugins/misc/misc.js
@@ -1,1 +1,143 @@
+/**
+ * Miscellaneous commands
+ */
 
+'use strict';
+/*eslint no-restricted-modules: [0]*/
+
+let moment = require('moment');
+let request = require('request');
+
+function clearRoom(room) {
+	let len = (room.log && room.log.length) || 0;
+	let users = [];
+	while (len--) {
+		room.log[len] = '';
+	}
+	for (let u in room.users) {
+		users.push(u);
+		Users.get(u).leaveRoom(room, Users.get(u).connections[0]);
+	}
+	len = users.length;
+	setTimeout(function () {
+		while (len--) {
+			Users.get(users[len]).joinRoom(room, Users.get(users[len]).connections[0]);
+		}
+	}, 1000);
+}
+
+exports.commands = {
+	gclearall: 'globalclearall',
+	globalclearall: function (target, room, user) {
+		if (!this.can('gdeclare')) return false;
+
+		for (let u in Users.users) {
+			Users.users[u].popup("All rooms are being clear.");
+		}
+		Rooms.rooms.forEach(clearRoom);
+	},
+
+	masspm: 'pmall',
+	pmall: function (target, room, user) {
+		if (!this.can('pmall')) return false;
+		if (!target) return this.parse('/help pmall');
+
+		let pmName = ' Server PM [Do not reply]';
+
+		Users.users.forEach(function (user) {
+			let message = '|pm|' + pmName + '|' + user.getIdentity() + '|' + target;
+			user.send(message);
+		});
+	},
+	pmallhelp: ["/pmall [message] - PM all users in the server."],
+
+	staffpm: 'pmallstaff',
+	pmstaff: 'pmallstaff',
+	pmallstaff: function (target, room, user) {
+		if (!this.can('forcewin')) return false;
+		if (!target) return this.parse('/help pmallstaff');
+
+		let pmName = ' Staff PM [Do not reply]';
+
+		Users.users.forEach(function (user) {
+			if (!user.isStaff) return;
+			let message = '|pm|' + pmName + '|' + user.getIdentity() + '|' + target;
+			user.send(message);
+		});
+	},
+	pmallstaffhelp: ["/pmallstaff [message] - Sends a PM to every staff member online."],
+
+	regdate: function (target, room, user) {
+		if (!this.runBroadcast()) return;
+		if (!target || !toId(target)) return this.parse('/help regdate');
+		let username = toId(target);
+		request('http://pokemonshowdown.com/users/' + username, function (error, response, body) {
+			if (error && response.statusCode !== 200) {
+				this.sendReplyBox(Chat.escapeHTML(target) + " is not registered.");
+				return room.update();
+			}
+			let regdate = body.split('<small>')[1].split('</small>')[0].replace(/(<em>|<\/em>)/g, '');
+			if (regdate === '(Unregistered)') {
+				this.sendReplyBox(Chat.escapeHTML(target) + " is not registered.");
+			} else if (regdate === '(Account disabled)') {
+				this.sendReplyBox(Chat.escapeHTML(target) + "'s account is disabled.");
+			} else {
+				this.sendReplyBox(Chat.escapeHTML(target) + " was registered on " + regdate.slice(7) + ".");
+			}
+			room.update();
+		}.bind(this));
+	},
+	regdatehelp: ["/regdate - Please specify a valid username."],
+
+	seen: function (target, room, user) {
+		if (!this.runBroadcast()) return;
+		if (!target) return this.parse('/help seen');
+		let targetUser = Users.get(target);
+		if (targetUser && targetUser.connected) return this.sendReplyBox(targetUser.name + " is <b>currently online</b>.");
+		target = Chat.escapeHTML(target);
+		let seen = Db('seen').get(toId(target));
+		if (!seen) return this.sendReplyBox(target + " has never been online on this server.");
+		this.sendReplyBox(target + " was last seen <b>" + moment(seen).fromNow() + "</b>.");
+	},
+	seenhelp: ["/seen - Shows when the user last connected on the server."],
+
+	tell: function (target, room, user, connection) {
+		if (!target) return this.parse('/help tell');
+		target = this.splitTarget(target);
+		let targetUser = this.targetUser;
+		if (!target) {
+			this.sendReply("You forgot the comma.");
+			return this.parse('/help tell');
+		}
+
+		if (targetUser && targetUser.connected) {
+			return this.parse('/pm ' + this.targetUsername + ', ' + target);
+		}
+
+		if (user.locked) return this.popupReply("You may not send offline messages when locked.");
+		if (target.length > 255) return this.popupReply("Your message is too long to be sent as an offline message (>255 characters).");
+
+		if (Config.tellrank === 'autoconfirmed' && !user.autoconfirmed) {
+			return this.popupReply("You must be autoconfirmed to send an offline message.");
+		} else if (!Config.tellrank || Config.groupsranking.indexOf(user.group) < Config.groupsranking.indexOf(Config.tellrank)) {
+			return this.popupReply("You cannot send an offline message because offline messaging is " +
+				(!Config.tellrank ? "disabled" : "only available to users of rank " + Config.tellrank + " and above") + ".");
+		}
+
+		let userid = toId(this.targetUsername);
+		if (userid.length > 18) return this.popupReply("\"" + this.targetUsername + "\" is not a legal username.");
+
+		let sendSuccess = Tells.addTell(user, userid, target);
+		if (!sendSuccess) {
+			if (sendSuccess === false) {
+				return this.popupReply("User " + this.targetUsername + " has too many offline messages queued.");
+			} else {
+				return this.popupReply("You have too many outgoing offline messages queued. Please wait until some have been received or have expired.");
+			}
+		}
+		return connection.send('|pm|' + user.getIdentity() + '|' +
+			(targetUser ? targetUser.getIdentity() : ' ' + this.targetUsername) +
+			"|/text This user is currently offline. Your message will be delivered when they are next online.");
+	},
+	tellhelp: ["/tell [username], [message] - Send a message to an offline user that will be received when they log in."],
+};
