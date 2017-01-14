aaf0d5f4c4fdf155d022ec6957de0cb0463252b5
SteelElite <charmsteel1@gmail.com>
2017-01-13 20:32:40 -0500

    Update away.js

--- plugins/plugins/away/away.js
+++ plugins/plugins/away/away.js
@@ -27,11 +27,6 @@ function parseStatus(text, encoding) {
 	return text;
 }
 
-let color = require('../plugins/utils/colors/hash');
-hashColor = function(name, bold) {
-	return (bold ? "<b>" : "") + "<font color=" + color(name) + ">" + (Users(name) && Users(name).connected && Users.getExact(name) ? Tools.escapeHTML(Users.getExact(name).name) : Tools.escapeHTML(name)) + "</font>" + (bold ? "</b>" : "");
-}
-
 exports.commands = {
 	away: function (target, room, user) {
 		if (!user.isAway && user.name.length > 15) return this.sendReply('Su nombre de usuario es demasiado largo para cualquier tipo de uso de este comando.');
