d67372f36dec85bf62c8322439f10d13e5b24759
SteelElite <charmsteel1@gmail.com>
2017-01-14 19:40:04 -0500

    Update custom.js

--- plugins/plugins/custom/custom.js
+++ plugins/plugins/custom/custom.js
@@ -207,24 +207,44 @@ exports.commands = {
 	},
 
 	roomlist: function (target, room, user) {
-		if (!this.can('roomlist')) return;
-		let rooms = Object.keys(Rooms.rooms);
-		let len = rooms.length;
-		let official = ['<b><font color="#1a5e00" size="2">Salas oficiales:</font></b><br><br>'];
-		let nonOfficial = ['<hr><b><font color="#000b5e" size="2">Salas no-oficiales:</font></b><br><br>'];
-		let privateRoom = ['<hr><b><font color="#5e0019" size="2">Salas privadas:</font></b><br><br>'];
-		while (len--) {
-			let _room = Rooms.rooms[rooms[(rooms.length - len) - 1]];
-			if (_room.type === 'chat') {
-				if (_room.isOfficial) {
-					official.push(('<a href="/' + _room.title + '" class="ilink">' + _room.title + '</a> |'));
-				} else if (_room.isPrivate) {
-					privateRoom.push(('<a href="/' + _room.title + '" class="ilink">' + _room.title + '</a> |'));
-				} else {
-					nonOfficial.push(('<a href="/' + _room.title + '" class="ilink">' + _room.title + '</a> |'));
+		let header = ['<b><font color="#1aff1a" size="2">Total users connected: ' + Rooms.global.userCount + '</font></b><br />'],
+			official = ['<b><font color="#ff9900" size="2"><u>Official Rooms:</u></font></b><br />'],
+			nonOfficial = ['<hr><b><u><font color="#005ce6" size="2">Public Rooms:</font></u></b><br />'],
+			privateRoom = ['<hr><b><u><font color="#ff0066" size="2">Private Rooms:</font></u></b><br />'],
+			groupChats = ['<hr><b><u><font color="#00b386" size="2">Group Chats:</font></u></b><br />'],
+			battleRooms = ['<hr><b><u><font color="#cc0000" size="2">Battle Rooms:</font></u></b><br />'];
+
+		let rooms = [];
+
+		Rooms.rooms.forEach(curRoom => {
+			if (curRoom.id !== 'global') rooms.push(curRoom.id);
+		});
+
+		rooms.sort();
+
+		for (let u in rooms) {
+			let curRoom = Rooms(rooms[u]);
+			if (curRoom.type === 'battle') {
+				battleRooms.push('<a href="/' + curRoom.id + '" class="ilink">' + Chat.escapeHTML(curRoom.title) + '</a> (' + curRoom.userCount + ')');
+			}
+			if (curRoom.type === 'chat') {
+				if (curRoom.isPersonal) {
+					groupChats.push('<a href="/' + curRoom.id + '" class="ilink">' + curRoom.id + '</a> (' + curRoom.userCount + ')');
+					continue;
+				}
+				if (curRoom.isOfficial) {
+					official.push('<a href="/' + toId(curRoom.title) + '" class="ilink">' + Chat.escapeHTML(curRoom.title) + '</a> (' + curRoom.userCount + ')');
+					continue;
+				}
+				if (curRoom.isPrivate) {
+					privateRoom.push('<a href="/' + toId(curRoom.title) + '" class="ilink">' + Chat.escapeHTML(curRoom.title) + '</a> (' + curRoom.userCount + ')');
+					continue;
 				}
 			}
+			if (curRoom.type !== 'battle') nonOfficial.push('<a href="/' + toId(curRoom.title) + '" class="ilink">' + curRoom.title + '</a> (' + curRoom.userCount + ')');
 		}
-		this.sendReplyBox(official.join(' ') + nonOfficial.join(' ') + privateRoom.join(' '));
-	}
+
+		if (!user.can('roomowner')) return this.sendReplyBox(header + official.join(' ') + nonOfficial.join(' '));
+		this.sendReplyBox(header + official.join(' ') + nonOfficial.join(' ') + privateRoom.join(' ') + (groupChats.length > 1 ? groupChats.join(' ') : '') + (battleRooms.length > 1 ? battleRooms.join(' ') : ''));
+	},
 };
