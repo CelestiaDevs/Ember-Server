54e1f94a8a5f3ab0e7fb73955ce9be1c5fddc780
SteelElite <charmsteel1@gmail.com>
2017-01-15 10:00:19 -0500

    Update away.js

--- plugins/plugins/away/away.js
+++ plugins/plugins/away/away.js
@@ -27,6 +27,11 @@ function parseStatus(text, encoding) {
 	return text;
 }
 
+var color = require('../config/color');
+hashColor = function(name, bold) {
+	return (bold ? "<b>" : "") + "<font color=" + color(name) + ">" + (Users(name) && Users(name).connected && Users.getExact(name) ? Tools.escapeHTML(Users.getExact(name).name) : Tools.escapeHTML(name)) + "</font>" + (bold ? "</b>" : "");
+}
+
 exports.commands = {
 	away: function (target, room, user) {
 		if (!user.isAway && user.name.length > 15) return this.sendReply('Your user name is too long to update your status.');
