81a869f5943f85b396ace368250fa310c4ffc542
SteelElite <charmsteel1@gmail.com>
2017-01-31 13:29:12 -0500

    Update custom.js

--- plugins/plugins/custom/custom.js
+++ plugins/plugins/custom/custom.js
@@ -236,13 +236,13 @@ exports.commands = {
 	},
 	
 	fleur : 'fleur',
-    fleur: function(target, room, user) {
-            if (!this.canBroadcast()) return;
+    	fleur: function(target, room, user) {
+            if (!this.runBroadcast()) return;
             this.sendReplyBox('<center><tbody><table height="100%" width="100%" padding:5px; style="background-image: url(&quot;http://i.imgur.com/fpc8bl1.jpg&quot;); background-size: cover" table border="0"><tbody><tr><td width="25%" height="100%"><center><img height="180" src="https://s29.postimg.org/u6brormg7/yozora_g1.gif">' +
                 '<a href="http://picosong.com/rbeq/" target="_blank"><img src="https://s27.postimg.org/nd0zdnvur/fleur_text.png" height="125" alt="Theme" /></a>' +
                 '<img src="https://s29.postimg.org/9ngvjp8iv/yozora_g2.gif" height="180"><br />' +
                 '<b>Catchphrase: </b>"What kind of style do you desire to change into? Mesmerize your identity with fashion and make up. Blossom into an elegant flower, so that you may live your life with freedom and happiness."</center>');
-    },
+    	},
 
 	roomlist: function (target, room, user) {
 		let header = ['<b><font color="#1aff1a" size="2">Total users connected: ' + Rooms.global.userCount + '</font></b><br />'],
